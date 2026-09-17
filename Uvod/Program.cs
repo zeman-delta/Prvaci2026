dbfdbefece1e2594a906a80d5a3fabee4460e89d
Martin Zeman <martin.zeman@freelo.io>
2026-09-17 09:57:38 +0200

    uvod ukazka

--- Uvod/Program.cs
+++ Uvod/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int[] pole = new int[30];
+
+        for (int i = 0; i < pole.Length; i++)
+        {
+            Random rnd = new Random();
+            
+            pole[i] = rnd.Next(20, 101);
+        }
+
+        
+        int soucet = 0;
+
+        for (int i = 0; i < pole.Length; i++)
+        {
+            Console.Write(pole[i] + "; ");
+            soucet = soucet + pole[i];
+        }
+
+        Console.WriteLine();
+        Console.WriteLine(soucet);
+
+
+        Console.WriteLine("Toto" + " je " + "nas " + "vysledek" + '8');
+        Console.WriteLine("Toto je nas vysledek");
+        
     }
 }
