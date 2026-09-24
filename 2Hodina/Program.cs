18051f80a7ffdf029f5d51f9fb6bc1547312302e
Martin Zeman <martin.zeman@freelo.io>
2026-09-24 11:06:10 +0200

    2. hodina (budeme dělat že jsme toho stihli více)

---
+++ 2Hodina/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _2Hodina;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Zadej 1. cislo:");
+        double a = double.Parse(Console.ReadLine());
+        Console.WriteLine("Zadej 2. cislo:");
+        double b = double.Parse(Console.ReadLine());
+
+        double c = a + b;
+
+        Console.WriteLine($"Součet je {c}");
+    }
+}
